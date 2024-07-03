dfc05aa337d10ceebe31c74fd0f520abd479f093
Gabriel Coelho <108440422+gabrielcoelho90@users.noreply.github.com>
2024-07-03 00:11:58 -0300

    Update water_block.rb

--- WaterBlock/water_block.rb
+++ WaterBlock/water_block.rb
@@ -8,24 +8,16 @@ def trap(height)
     run = true
     while run
       left_height = height.index { |item| item > 0 }
-      p left_height
       right_height = height.select { |item| item > 0}.last
-      p right_height
       closed_container = height[left_height..right_height]
-      p "closed container #{closed_container}"
       block_of_water = closed_container.select { |item| item <= 0 }.size
-      p "block water #{block_of_water}"
       water << block_of_water
-      p "water #{water}"
       reduce_level = closed_container.map { |item| item - 1 }
       stop = reduce_level.select{|i| i > 0}
       run = false if reduce_level.all? { |item| item <= 0 } || stop.size == 1
-      p "reduce level #{reduce_level}"
       height = reduce_level
     end
   end
-  p water.sum
+  water.sum
 end
 
-
-trap(height)
